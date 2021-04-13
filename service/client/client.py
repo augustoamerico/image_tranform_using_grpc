4dd319a2b4539631a92ef2823a575772c6d9c46a
Tiago Santos <tiago.santos@definedcrowd.com>
2021-04-14 00:59:13 +0100

    first iteration on the service: from first iteration of the model to first (client,server)

---
+++ service/client/client.py
@@ -0,0 +1,79 @@
+# flake8: noqa
+import uuid
+import datetime
+import logging
+import json
+from flask import Flask, flash, request, redirect, make_response, render_template_string
+
+import grpc
+import ImageService_pb2 as service
+import ImageService_pb2_grpc as rpc
+from google.protobuf.json_format import MessageToJson
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', '.gif'}
+CHUNK_SIZE = 1024 * 1024  # decrease the value here to evaluate memory usage
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'selected_files' not in request.files:
+            flash("No 'selected_files' part")
+            return redirect(request.url)
+        file = request.files["selected_files"]
+        #for i, file in enumerate(selected_files, 1):
+        if file.filename == '':
+            flash('You must select at least one file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            fileid = f'{datetime.datetime.utcnow().isoformat()}-{uuid.uuid4()}'
+
+            #def upload_request_generator():  # this generates our grpc `stream ImageUploadRequest`
+            my_result = []
+            while True:
+                b = file.read(CHUNK_SIZE)
+                if b:
+                    result = service.ImageUploadRequest(Content=b, Id=fileid, StatusCode=service.ImageUploadStatusCode.InProgress)  # noqa
+                    logging.info("Sending...")
+                    my_result.append(result)
+                else:
+                    result = service.ImageUploadRequest(Id=fileid, StatusCode=service.ImageUploadStatusCode.Ok)  # noqa
+                    logging.info("...Sent")
+                    my_result.append(result)
+                    logging.info(f"len of list is {len(my_result)}")
+                    break
+            my_result = iter(my_result)
+
+            result = stub.Upload(my_result)
+            logging.info(result)
+            logging.info(f'file {file.filename} was upload successfully')
+            #logging.info(result)
+            #results.append(MessageToJson(result))
+            response = make_response(result.Content)
+            response.headers.set('Content-Type', 'image/jpeg')
+            response.headers.set(
+                'Content-Disposition', 'attachment', filename=f'{file.filename}_googlyeyzed.jpg')
+            return response
+    return render_template_string('''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=selected_files>
+      <input type=submit value=Upload>
+    </form>
+    ''', json_response=request.args.get('json'))
+
+
+if __name__ == "__main__":
+    channel = grpc.insecure_channel('localhost:22223')
+    stub = rpc.ImageServiceStub(channel)
+    app.run(host='0.0.0.0')
---
+++ service/server/server.py
@@ -0,0 +1,56 @@
+from concurrent import futures
+import os
+import io
+from collections import defaultdict
+import logging
+
+import requests
+import grpc
+
+import ImageService_pb2 as service
+import ImageService_pb2_grpc as rpc
+
+from domain.image_googlyeyz import GooglyEyezer
+
+class ImageServiceServer(rpc.ImageServiceServicer):  # inheriting here from the protobuf rpc file which is generated
+
+    def __init__(self):
+        self.images = defaultdict(io.BytesIO)
+        logging.info("successfuly created the images store")
+        self.googlyeyezer = GooglyEyezer()
+
+    def Upload(self, request_iterator, context):
+        i = 0
+        for request in request_iterator:
+            logging.info(f"Iteration: {i}")
+            if request.StatusCode == service.ImageUploadStatusCode.InProgress:
+                logging.info(f"bytes: {request.Content[0:10]}...")
+                logging.info(f'> Req {request.Id} - receiving image')
+                self.images[request.Id].write(request.Content)
+                
+                result = service.ImageUploadResponse(Id=request.Id, StatusCode=service.ImageUploadStatusCode.Ok, Message='waiting for more')
+
+            if request.StatusCode == service.ImageUploadStatusCode.Ok and not request.Content:
+                logging.info(f'> {request.Id} - received image')
+                logging.info(f'> {request.Id} - googlyeyezing')
+                image = self.images[request.Id].getvalue()
+                logging.info(f"bytes: {image[0:10]}...")
+                image_googlyeyezed = self.googlyeyezer.apply(io.BytesIO(image), "jpg")
+
+                del self.images[request.Id]
+
+                logging.info(f'> {request.Id} - googlyeyezed')
+                result = service.ImageUploadResponse(Content= image_googlyeyezed.read(), Id=request.Id, StatusCode=service.ImageUploadStatusCode.Ok, Message="Processed")
+                return result
+            i+=1
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    port = 22223
+    grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    rpc.add_ImageServiceServicer_to_server(ImageServiceServer(), grpc_server)
+    logging.info(f'Starting server. Listening at {port}...')
+    grpc_server.add_insecure_port(f'[::]:{port}')
+    grpc_server.start()
+    grpc_server.wait_for_termination()
